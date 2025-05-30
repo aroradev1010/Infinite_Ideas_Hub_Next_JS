56a0f0587603aa87a691e6c17ecd4460cfff9626
Dev Arora <aroradev1010@gmail.com>
2025-05-30 16:48:44 +0530

    project setup

---
+++ app/authors/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Authors</div>;
+};
+
+export default page;
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="textPrimary">
+      <div className="">Homepage</div>
+    </div>
+  );
+}
