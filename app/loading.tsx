2c84d02e88083afed6db0e0a5115714d24f1aad9
Dev Arora <aroradev1010@gmail.com>
2025-06-04 02:40:37 +0530

    add loader

---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+// app/loading.tsx
+export default function Loading() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="w-10 h-10 border-4 border-t-transparent border-white rounded-full animate-spin"></div>
+    </div>
+  );
+}
