3989d9a979bc894ce34db37dc0240f8a53a07659
Dev Arora <aroradev1010@gmail.com>
2025-06-04 18:23:02 +0530

    update 404 Not Found page UI

---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen  text-white px-4">
+      <div className="text-center">
+        <h1 className="text-6xl font-extrabold mb-4">404</h1>
+        <p className="text-xl mb-8">
+          Oops! The page you're looking for doesn't exist.
+        </p>
+        <Link
+          href="/"
+          className="inline-block px-6 py-3 bg-primary hover:bg-primary/80 text-white font-bold rounded-lg transition duration-300"
+        >
+          Go Back Home
+        </Link>
+      </div>
+    </div>
+  );
+}
